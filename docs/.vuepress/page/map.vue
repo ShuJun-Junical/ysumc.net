488e64aacc818e2dec3c1fc87bdb2885afe861f8
纾浚 <Junical@email.cn>
2025-04-16 23:35:20 +0800

    部分标点更新，响应式细节微调

--- docs/.vuepress/page/map.vue
+++ docs/.vuepress/page/map.vue
@@ -23,8 +23,8 @@ const colorList = {
   orange: '#d36839',
   gray: '#777777',
   redDark: '#9c292d',
-  gate: '#1f296a'
-}
+  gate: '#1f296a',
+};
 
 function onZoom(zoom) {
   if (zoom <= 1) {
@@ -32,10 +32,13 @@ function onZoom(zoom) {
     if (barIsTitle.value && showBar.value) return;
     showBar.value = false;
     const barIsTitleOld = barIsTitle.value;
-    setTimeout(() => {
-      barIsTitle.value = true;
-      showBar.value = true;
-    }, barIsTitleOld ? 0 : 300);
+    setTimeout(
+      () => {
+        barIsTitle.value = true;
+        showBar.value = true;
+      },
+      barIsTitleOld ? 0 : 300
+    );
   } else {
     setNavBar(true);
     if (!barIsTitle.value) return;
@@ -48,31 +51,39 @@ function onPointclick(point) {
   const showBarOld = showBar.value;
   if (showBarOld && point.title === barTitle.value) return;
   showBar.value = false;
-  setTimeout(() => {
-    barTitle.value = point.title || '';
-    barTags.value = point.label || [];
-    barText.value = point.text || '';
-    pointColor.value = point.color || 'blue';
-    showBar.value = true;
-  }, showBarOld ? 300 : 0);
+  setTimeout(
+    () => {
+      barTitle.value = point.title || '';
+      barTags.value = point.label || [];
+      barText.value = point.text || '';
+      pointColor.value = point.color || 'blue';
+      showBar.value = true;
+    },
+    showBarOld ? 300 : 0
+  );
 }
 
 function onMapclick(latlng) {
-  console.log(latlng)
+  console.log(latlng);
   if (barIsTitle.value) return;
-  showBar.value ? showBar.value = false : null;
+  showBar.value ? (showBar.value = false) : null;
 }
 </script>
 
 <template>
   <div class="h-dvh">
     <MapCanva @zoom="onZoom" @pointclick="onPointclick" @mapclick="onMapclick" />
-    <div
-      class="w-dvw fixed shadow-2xl bottom-0 bg-white/90 backdrop-blur py-6 md:py-8 px-4 md:px-16 z-[10000] flex flex-col md:flex-row md:items-center md:justify-between transition-transform duration-200"
-      :class="{ 'translate-y-full': !showBar, 'translate-y-0': showBar }">
+    <div class="w-dvw fixed shadow-2xl bottom-0 bg-white/90 backdrop-blur
+        py-6 md:py-8 px-4 md:px-16 lg:px-20 xl:px-32 z-[10000]
+        flex flex-col md:flex-row md:items-center md:justify-between
+        transition-transform duration-200" :class="{ 'translate-y-full': !showBar, 'translate-y-0': showBar }">
       <div>
         <h1 class="font-ysumc text-xl md:text-2xl mb-2 md:mb-4">
-          {{ barIsTitle ? '《像素燕大：燕山大学复原计划》全导览图（2024）' : barTitle }}
+          {{
+            barIsTitle
+              ? '《像素燕大：燕山大学复原计划》全导览图（2024）'
+              : barTitle
+          }}
         </h1>
         <p v-show="barIsTitle" class="text-sm md:text-base">
           本图按燕山大学2024年5月时的燕山大学复原，在当时未建成的建筑按已完成计，蓝本为其效果图。<br />
@@ -88,7 +99,7 @@ function onMapclick(latlng) {
           </span>
         </div>
       </div>
-      <div v-show="barIsTitle" class="hidden lg:flex flex-row items-center gap-8 ">
+      <div v-show="barIsTitle" class="hidden lg:flex flex-row items-center gap-8">
         <div>
           <img src="@/asset/icons/left-click.svg" class="h-16 mx-auto" />
           <p class="mt-4 text-sm">按住左键拖动以移动</p>
