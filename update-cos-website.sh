533414d74920f11bdb94900672688939064fde13
纾浚 <Junical@email.cn>
2026-07-28 19:24:57 +0800

    github workflow

--- update-cos-website.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# ~/bin/coscli rm cos://ysumc-club-1301539318/ -r -f
-
-# ~/bin/coscli cp ~/Code/website/ysumc.net/docs/.vuepress/dist cos://ysumc-club-1301539318/ -r --routines 64
-
-~/bin/coscli sync ~/Code/website/ysumc.net/docs/.vuepress/dist cos://ysumc-club-1301539318/ -r --delete --routines 64
